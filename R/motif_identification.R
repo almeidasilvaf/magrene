be119e556927783732ae58e2d9339979fe4aa768
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2022-07-22 22:08:18 +0200

    Added support for BiocParallel + added check to remove edges when none of the partners were in the paralogs data frame + added option to input lambdas to find_delta() and find_bifan(), so lambdas don't need to be calculated again

--- R/motif_identification.R
+++ R/motif_identification.R
@@ -6,10 +6,13 @@
 #' targets in column 2.
 #' @param paralogs A 2-column data frame with gene IDs for each paralog
 #' in the paralog pair.
+#' @param bp_param BiocParallel back-end to be used.
+#' Default: BiocParallel::SerialParam()
 #'
 #' @return A character vector with lambda motifs represented 
 #' in the format \strong{target1<-regulator->target2}.
 #' @importFrom utils combn
+#' @importFrom BiocParallel bplapply SerialParam
 #' @export
 #' @rdname find_lambda
 #' @examples 
@@ -18,7 +21,15 @@
 #' edgelist <- gma_grn[500:1000, 1:2] # reducing for test purposes
 #' paralogs <- gma_paralogs[gma_paralogs$type == "WGD", 1:2]
 #' motifs <- find_lambda(edgelist, paralogs)
-find_lambda <- function(edgelist = NULL, paralogs = NULL) {
+find_lambda <- function(edgelist = NULL, paralogs = NULL,
+                        bp_param = BiocParallel::SerialParam()) {
+    
+    names(edgelist) <- c("Node1", "Node2")
+    names(paralogs) <- c("Dup1", "Dup2")
+
+    # Removing edges where target is not in paralogs df
+    all_paralogs <- unique(c(paralogs$Dup1, paralogs$Dup2))
+    edgelist <- edgelist[edgelist$Node2 %in% all_paralogs, ]
     
     pvec <- paste0(paralogs[,1], paralogs[,2]) # paralogs vector
     
@@ -30,7 +41,7 @@ find_lambda <- function(edgelist = NULL, paralogs = NULL) {
     
     # Create a list of all combinations of partners for each TF, then
     # count how many combinations include a paralog pair
-    motifs <- unlist(lapply(tfs_and_interactions, function(x) {
+    motifs <- unlist(bplapply(tfs_and_interactions, function(x) {
         partners <- unique(x[, 2])
         comb <- utils::combn(partners, 2, simplify = FALSE)
         comb1 <- unlist(lapply(comb, function(x) paste0(x[1], x[2])))
@@ -49,7 +60,7 @@ find_lambda <- function(edgelist = NULL, paralogs = NULL) {
             edges <- NULL
         }
         return(edges)
-    }))
+    }, BPPARAM = bp_param))
     return(motifs)
     
 }
@@ -58,11 +69,19 @@ find_lambda <- function(edgelist = NULL, paralogs = NULL) {
 #' Find delta motifs
 #'
 #' @param edgelist A 2-column data frame with regulators in column 1 and
-#' targets in column 2.
+#' targets in column 2. It can be ignored if you give lambda motifs
+#' to parameter \strong{lambda_vec} (recommended).
 #' @param paralogs A 2-column data frame with gene IDs for each paralog
-#' in the paralog pair.
+#' in the paralog pair. It can be ignored if you give lambda motifs
+#' to parameter \strong{lambda_vec} (recommended).
 #' @param edgelist_ppi A 2-column data frame with IDs of genes that encode
 #' each protein in the interacting pair.
+#' @param lambda_vec A character of lambda motifs as returned 
+#' by \code{find_lambda()}. If this is NULL, this function will find lambda
+#' motifs from \strong{edgelist} and \strong{paralogs} first. Passing 
+#' previously identified lambda motifs will make this function much faster.
+#' @param bp_param BiocParallel back-end to be used.
+#' Default: BiocParallel::SerialParam().
 #' 
 #' @return A character vector with lambda motifs represented 
 #' in the format \strong{target1<-regulator->target2}.
@@ -75,15 +94,20 @@ find_lambda <- function(edgelist = NULL, paralogs = NULL) {
 #' edgelist <- gma_grn[500:1000, 1:2] # reducing for test purposes
 #' paralogs <- gma_paralogs[gma_paralogs$type == "WGD", 1:2]
 #' edgelist_ppi <- gma_ppi
-#' motifs <- find_delta(edgelist, paralogs, edgelist_ppi)
+#' lambda_vec <- find_lambda(edgelist, paralogs)
+#' motifs <- find_delta(edgelist_ppi = edgelist_ppi, lambda_vec = lambda_vec)
 find_delta <- function(edgelist = NULL, paralogs = NULL,
-                       edgelist_ppi = NULL) {
+                       edgelist_ppi = NULL, lambda_vec = NULL,
+                       bp_param = BiocParallel::SerialParam()) {
     
     ivec <- paste0(edgelist_ppi[, 1], edgelist_ppi[, 2]) # PPI vector
     
     # Look for lambda motifs for which targets interact (delta)
-    lambda <- find_lambda(edgelist, paralogs)
-    motifs <- unlist(lapply(lambda, function(x) {
+    lambda <- lambda_vec
+    if(is.null(lambda_vec)) {
+        lambda <- find_lambda(edgelist, paralogs)
+    }
+    motifs <- unlist(bplapply(lambda, function(x) {
         t1 <- gsub("<-.*", "", x)
         t2 <- gsub(".*->", "", x)
         
@@ -96,8 +120,7 @@ find_delta <- function(edgelist = NULL, paralogs = NULL,
         if(check != 0) {
             motif <- x
         }
-    }))
-    motifs
+    }, BPPARAM = bp_param))
     return(motifs)
 }
 
@@ -108,10 +131,13 @@ find_delta <- function(edgelist = NULL, paralogs = NULL,
 #' targets in column 2.
 #' @param paralogs A 2-column data frame with gene IDs for each paralog
 #' in the paralog pair.
+#' @param bp_param BiocParallel back-end to be used.
+#' Default: BiocParallel::SerialParam().
 #'
 #' @return A character vector with V motifs represented 
 #' in the format \strong{regulator1->target<-regulator2}.
 #' 
+#' @importFrom BiocParallel bplapply SerialParam
 #' @importFrom utils combn
 #' @export
 #' @rdname find_v
@@ -121,13 +147,19 @@ find_delta <- function(edgelist = NULL, paralogs = NULL,
 #' edgelist <- gma_grn[2000:4000, 1:2] # reducing for test purposes
 #' paralogs <- gma_paralogs[gma_paralogs$type == "WGD", 1:2]
 #' motifs <- find_v(edgelist, paralogs)
-find_v <- function(edgelist = NULL, paralogs = NULL) {
+find_v <- function(edgelist = NULL, paralogs = NULL,
+                   bp_param = BiocParallel::SerialParam()) {
     
-    pvec <- paste0(paralogs[,1], paralogs[,2]) # paralogs vector
+    names(edgelist) <- c("Node1", "Node2")
+    names(paralogs) <- c("Dup1", "Dup2")
+    
+    # Removing edges where target is not in paralogs df
+    all_paralogs <- unique(c(paralogs$Dup1, paralogs$Dup2))
+    edgelist <- edgelist[edgelist$Node1 %in% all_paralogs, ]
+     
+    pvec <- paste0(paralogs$Dup1, paralogs$Dup2) # paralogs vector
     
     # Create list of TFs and its interacting partners
-    names(edgelist) <- c("Node1", "Node2")
-    edgelist <- edgelist[!duplicated(edgelist$Node1, edgelist$Node2), ]
     targets_and_tfs <- split(edgelist, edgelist$Node2)
     len <- vapply(targets_and_tfs, nrow, numeric(1))
     targets_and_tfs <- targets_and_tfs[len >= 2]
@@ -136,7 +168,7 @@ find_v <- function(edgelist = NULL, paralogs = NULL) {
     # count how many combinations include a paralog pair
     motifs <- NULL
     if(length(targets_and_tfs) > 0) {
-        motifs <- unlist(lapply(targets_and_tfs, function(x) {
+        motifs <- unlist(bplapply(targets_and_tfs, function(x) {
             tfs <- unique(x[, 1])
             comb <- utils::combn(tfs, 2, simplify = FALSE)
             comb1 <- unlist(lapply(comb, function(x) paste0(x[1], x[2])))
@@ -155,7 +187,7 @@ find_v <- function(edgelist = NULL, paralogs = NULL) {
                 edges <- NULL
             }
             return(edges)
-        }))
+        }, BPPARAM = bp_param))
     }
     return(motifs)
 }
@@ -163,10 +195,12 @@ find_v <- function(edgelist = NULL, paralogs = NULL) {
 
 #' Find V motifs in protein-protein interactions
 #'
-#' @param edgelist A 2-column data frame with regulators in column 1 and
-#' targets in column 2.
+#' @param edgelist A 2-column data frame with protein 1 in column 1 and
+#' protein 2 in column 2.
 #' @param paralogs A 2-column data frame with gene IDs for each paralog
 #' in the paralog pair.
+#' @param bp_param BiocParallel back-end to be used.
+#' Default: BiocParallel::SerialParam().
 #'
 #' @return A character vector with V motifs represented 
 #' in the format \strong{paralog1-partner-paralog2}.
@@ -174,6 +208,7 @@ find_v <- function(edgelist = NULL, paralogs = NULL) {
 #' @details This function aims to find the number of paralogous gene
 #' pairs that share an interaction partner.
 #' 
+#' @importFrom BiocParallel bplapply SerialParam
 #' @importFrom utils combn
 #' @export
 #' @rdname find_ppi_v
@@ -182,21 +217,27 @@ find_v <- function(edgelist = NULL, paralogs = NULL) {
 #' data(gma_paralogs)
 #' edgelist <- gma_ppi
 #' paralogs <- gma_paralogs[gma_paralogs$type == "WGD", 1:2]
-#' motifs <- find_ppi_v(edgelist, paralogs)
-find_ppi_v <- function(edgelist = NULL, paralogs = NULL) {
+#' motifs <- find_ppi_v(edgelist, paralogs) 
+find_ppi_v <- function(edgelist = NULL, paralogs = NULL,
+                       bp_param = BiocParallel::SerialParam()) {
     
-    pvec <- paste0(paralogs[,1], paralogs[,2]) # paralogs vector
+    names(edgelist) <- c("Node1", "Node2")
+    names(paralogs) <- c("Dup1", "Dup2")
+    
+    # Removing edges where target is not in paralogs df
+    all_paralogs <- unique(c(paralogs$Dup1, paralogs$Dup2))
+    edgelist <- edgelist[edgelist$Node1 %in% all_paralogs, ]
+     
+    pvec <- paste0(paralogs$Dup1, paralogs$Dup2) # paralogs vector
     
     # Create list of proteins and their interacting partners
-    names(edgelist) <- c("Node1", "Node2")
-    edgelist <- edgelist[!duplicated(edgelist$Node1, edgelist$Node2), ]
     targets_and_p <- split(edgelist, edgelist$Node2)
     len <- vapply(targets_and_p, nrow, numeric(1))
     targets_and_p <- targets_and_p[len >= 2]
     
     motifs <- NULL
     if(length(targets_and_p) > 0) {
-        motifs <- unlist(lapply(targets_and_p, function(x) {
+        motifs <- unlist(bplapply(targets_and_p, function(x) {
             p <- unique(x[, 1])
             if(length(p) > 1) {
                 comb <- utils::combn(p, 2, simplify = FALSE)
@@ -219,7 +260,7 @@ find_ppi_v <- function(edgelist = NULL, paralogs = NULL) {
                 edges <- NULL
             }
             return(edges)
-        }))
+        }, BPPARAM = bp_param))
     }
     return(motifs)
 }
@@ -228,13 +269,21 @@ find_ppi_v <- function(edgelist = NULL, paralogs = NULL) {
 #' Find bifan motifs
 #'
 #' @param edgelist A 2-column data frame with regulators in column 1 and
-#' targets in column 2.
+#' targets in column 2. It can be ignored if you give lambda motifs
+#' to parameter \strong{lambda_vec} (recommended).
 #' @param paralogs A 2-column data frame with gene IDs for each paralog
 #' in the paralog pair.
+#' @param lambda_vec A character of lambda motifs as returned 
+#' by \code{find_lambda()}. If this is NULL, this function will find lambda
+#' motifs from \strong{edgelist} and \strong{paralogs} first. Passing 
+#' previously identified lambda motifs will make this function much faster.
+#' @param bp_param BiocParallel back-end to be used.
+#' Default: BiocParallel::SerialParam().
 #'
 #' @return A character vector with bifan motifs represented 
 #' in the format \strong{regulator1, regulator2->target1, target2}.
 #' 
+#' @importFrom BiocParallel bplapply SerialParam
 #' @importFrom utils combn
 #' @export
 #' @rdname find_bifan
@@ -249,16 +298,22 @@ find_ppi_v <- function(edgelist = NULL, paralogs = NULL) {
 #'            duplicate2 = "Glyma.08G116700")
 #' )
 #' bifan <- find_bifan(edgelist, paralogs)
-find_bifan <- function(edgelist = NULL, paralogs = NULL) {
+find_bifan <- function(edgelist = NULL, paralogs = NULL,
+                       lambda_vec = NULL,
+                       bp_param = BiocParallel::SerialParam()) {
     
     pvec <- paste0(paralogs[,1], paralogs[,2]) # paralogs vector
     
     # Find lambda motifs and compare them pairwise to look for shared neighbors
-    lambdas <- find_lambda(edgelist, paralogs)
+    lambdas <- lambda_vec
+    if(is.null(lambda_vec)) {
+        lambdas <- find_lambda(edgelist, paralogs)
+    }
+    
     final_motifs <- NULL
     if(length(lambdas) > 1) {
         comb <- utils::combn(names(lambdas), 2, simplify = FALSE)
-        final_motifs <- unlist(lapply(comb, function(x) {
+        final_motifs <- unlist(bplapply(comb, function(x) {
             motif <- NULL
             tf1 <- x[1]
             tf2 <- x[2]
@@ -285,7 +340,7 @@ find_bifan <- function(edgelist = NULL, paralogs = NULL) {
             }
             
             return(motif)
-        }))
+        }, BPPARAM = bp_param))
     }
     return(final_motifs)
 }
